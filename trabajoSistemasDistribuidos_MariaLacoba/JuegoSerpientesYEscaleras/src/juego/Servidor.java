ee933b3453eea336d3c53adb383ec33bf1629fc3
malacoba <72509647+malacoba@users.noreply.github.com>
2023-12-10 17:10:27 +0100

    Entrega

---
+++ trabajoSistemasDistribuidos_MariaLacoba/JuegoSerpientesYEscaleras/src/juego/Servidor.java
@@ -0,0 +1,58 @@
+//María Lacoba Martinez
+package juego;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//clase servidor que cuando haya dos jugadores haga que empiece el juego
+public class Servidor {
+	public static void main(String[]args) {
+		ExecutorService pool = Executors.newFixedThreadPool(1);
+		List<InformacionJugador> informacion = new ArrayList<InformacionJugador>();
+		try(ServerSocket ss = new ServerSocket(8000)){
+			int c =0;
+			Socket cliente=null;
+			while(true) {
+				try {
+					while(informacion.size()<2) {
+						cliente= ss.accept();
+						DataInputStream br = new DataInputStream(cliente.getInputStream());
+						DataOutputStream bw = new DataOutputStream(cliente.getOutputStream());
+						bw.writeBytes("Espere a que empiece el juego"+ "\n");
+						bw.flush();
+						String nombre =br.readLine();
+						informacion.add(new InformacionJugador(nombre,bw,br));
+						
+					}
+					
+					if(c ==0) {
+						pool.execute(new ManejadorJugadores(informacion));
+						c++;
+					}
+					Socket cl= ss.accept();
+				
+				}catch(IOException ex) {
+					ex.printStackTrace();
+					if(cliente!=null) {
+						cliente.close();
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+}
---
+++ trabajoSistemasDistribuidos_MariaLacoba/Tablero.java
@@ -0,0 +1,5 @@
+package th1;
+
+public class Tablero {
+	private int mery;
+}
